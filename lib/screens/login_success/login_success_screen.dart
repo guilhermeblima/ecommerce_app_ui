e24d3ce0a0bd72ee2341121b20496661e79a9222
Gui Lima <glima@NZPC.CI.ORG>
2021-01-08 11:18:17 +1300

    ecommerce app from Flutter way

---
+++ lib/screens/login_success/login_success_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/login_success/components/body.dart';
+
+class LoginSuccessScreen extends StatelessWidget {
+  static String routeName = "/login_success";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: SizedBox(),
+        title: Text("Login Success"),
+      ),
+      body: Body(),
+    );
+  }
+}
